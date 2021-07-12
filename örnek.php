f6c42916d2d3bae62469912be6db403e2802fbc4
CemBalbay <70141107+CemBalbay@users.noreply.github.com>
2021-07-12 23:33:02 +0300

    Add files via upload

---
+++ örnek.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+//integer
+$int1=196;//sayısal değer için
+$int2=-36;//sayısal değer için
+echo $int1;
+echo "<br>";
+echo $int2;
+*/
+
+
+/*
+//string
+$string1="istanbul";//karakter atamak için
+$string2="köln";//karakter atamak için
+echo $string1;
+echo "<br>";
+echo $string2;    
+*/
+
+
+/*
+//fload
+$fload1="58.60";//noktalı sayısal değer için
+$fload2="128.000";//noktalı sayısal değer için
+echo $fload1;
+echo "<br>";
+echo $fload2;
+*/
+
+/*
+//bool
+$bool1= true;//doğru yanlış değer için
+$bool= false;//doğru yanlış değer için
+echo $bool1;
+*/
+
+/*
+//array
+$şehir=array("İstanbul","Ankara","İzmir");//birden fazla değer saklamak için
+echo $şehir[0];
+echo "<br>";
+echo $şehir[1];
+*/
+
+/*
+//object(verileri ve bilgileri depolayan bir veri türüdür.)
+class Adres 
+{  function Adres()
+    
+    {
+        $this->sehir = "Ankara";
+        $this->ilce = "Çankaya";
+        $this->alan = "268 km²";
+        $this->rakim = "986 m";
+    }
+}
+// nesne oluştur
+$nesne = new Adres();//newAnahtar kelime kullanılarak bir sınıfın nesneleri oluşturulur .
+
+// nesne özelliklerini göster
+echo $nesne->sehir;
+echo "<br>";
+echo $nesne->ilce;
+echo "<br>";
+echo $nesne->alan;
+echo "<br>";
+echo $nesne->rakim;
+*/
+
+?>
