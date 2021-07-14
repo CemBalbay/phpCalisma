83e19ed6a17ceadbf94a0660b4445216f435bc2c
CemBalbay <70141107+CemBalbay@users.noreply.github.com>
2021-07-14 12:58:29 +0300

    Add files via upload

---
+++ function.php
@@ -0,0 +1,58 @@
+<?php
+//echo "cem";
+
+/*
+$a = "10";
+$b = "110";
+var_dump($a == $b);
+//echo $a==$b ? true : false;
+*/
+
+/*
+$yıl = 2021;
+$dogum_yıl = 2003;
+$yas = $yıl - $dogum_yıl;   
+echo $yas;
+*/
+
+
+/*
+function cıkar($yıl, $dogum_yıl) {
+
+   $yas = $yıl + $;
+
+   return $yas;
+
+}
+
+echo cıkar(5, 10);
+
+*/
+
+
+
+/*
+function Ort(array $numbers) {
+    $sum = array_sum($numbers);//sum değerlerin toplamını hesaplar
+    $count = count($numbers);//count eleman sayısını döndürür
+    return $sum/$count;
+
+}
+$a = [50,100,80,4];
+echo Ort($a)
+*/
+
+
+
+
+/*
+function name($surname) {
+    echo "$surname balbay";
+}
+
+name("cem");
+*/
+
+
+
+?>
